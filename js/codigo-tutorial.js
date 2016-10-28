e7c2701abf9d470ff87f9ec407c3e83d414e2ec1
Moisés Mena <Moiss Mena>
2016-10-28 09:21:36 -0600

    pagina de tutorial agregado menu tutorial implementado

--- js/codigo-tutorial.js
+++ js/codigo-tutorial.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
-    $('.flexslider').flexslider({
-            animation: "slide"
-        });
-
     $(window).scroll(function () {
-          
+        
         if ($(this).scrollTop() > 0) {
             $('header').addClass('header2');
         } else {
